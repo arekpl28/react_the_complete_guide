3ef77408983e29c5e032c54c698f3035bf5ed1c9
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-06-27 19:06:50 +0200

    Set player name & working with state

--- 02-tic-tac-toe/src/components/Player.jsx
+++ 02-tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input></input>;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }

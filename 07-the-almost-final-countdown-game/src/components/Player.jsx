7ee818213dfcc6c67411c0de09d0c6d7265eceef
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-07-06 17:37:01 +0200

    User input wit state

--- 07-the-almost-final-countdown-game/src/components/Player.jsx
+++ 07-the-almost-final-countdown-game/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+  function handleClick() {
+    setSubmitted(!submitted);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "enknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          value={enteredPlayerName}
+          onChange={(e) => handleChange(e)}
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );

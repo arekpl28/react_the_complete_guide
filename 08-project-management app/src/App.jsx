371c1d35a487ddb686de5916f50e8885f4deda9c
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-07-07 19:41:31 +0200

    Splitting components & using tailwind css

--- 08-project-management app/src/App.jsx
+++ 08-project-management app/src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [active, setActive] = useState(false);
+  function handleClick() {
+    setActive(true);
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onClick={handleClick} />
+      {active ? <NewProject /> : <NoProjectSelected onClick={handleClick} />}
     </main>
   );
 }

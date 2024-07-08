066e2e85a1dc34ef833001aa38e1450b6b3bf0f6
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-07-08 19:48:17 +0200

    Making selectable & viewing projects details

---
+++ 08-project-management app/src/components/SelectedProject.jsx
@@ -0,0 +1,30 @@
+import Button from "./Button";
+
+function SelectedProject({ project }) {
+  const formattedDate = new Date(project.dueDate).toLocaleDateString("pl-PL", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <div className="w-[35rem] mt-16">
+      <header className="pb-4 mb-4 border-b-4 border-stone-300">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-stone-600 mb-2">
+            {project.title}
+          </h1>
+          <Button>Delete</Button>
+          {/* <button>Delete</button> */}
+        </div>
+        <p className="mb-4 text-stone-400">{formattedDate}</p>
+        <p className="text-stone-600 whitespace-pre-wrap">
+          {project.description}
+        </p>
+      </header>
+      TASKS
+    </div>
+  );
+}
+
+export default SelectedProject;

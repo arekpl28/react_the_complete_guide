f06d03dc94d46fddb5f4b8ba3a3f4f530de37a73
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-07-13 19:39:16 +0200

    Splitting & add answers & question components

--- 11-react-quiz/src/components/QuestionTimer.jsx
+++ 11-react-quiz/src/components/QuestionTimer.jsx
@@ -7,18 +7,14 @@ function QuestionTimer({ timeout, onTimeout }) {
     const timer = setTimeout(() => {
       onTimeout();
     }, timeout);
-    return () => {
-      clearTimeout(timer);
-    };
+    return () => clearTimeout(timer);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;

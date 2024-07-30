ebe17bcdecfb1688a7ab93102201e2ecfbe85dfa
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-07-30 19:30:56 +0200

    Handling form submission & validation

--- 17-food-order-app/src/components/Checkout.jsx
+++ 17-food-order-app/src/components/Checkout.jsx
@@ -19,12 +19,21 @@ function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    console.log(customerData);
+  }
+
   return (
     <Modal
       open={userProgressCtx.progress === "checkout"}
       onClose={handleCloseCheckout}
     >
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name" />

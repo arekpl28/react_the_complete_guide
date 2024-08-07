6a024d83f3b62fac380b3975f7362c3532767e7e
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-07 20:53:38 +0200

    Adding rest redux

--- 20-redux-side-effects-async/src/store/cart-slice.js
+++ 20-redux-side-effects-async/src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
@@ -10,9 +10,11 @@ createSlice({
     addItemToCart(state, actions) {
       const newItem = actions.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -23,6 +25,21 @@ createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, actions) {
+      const id = actions.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;

b1201494a2c67f6e2414b39fbd3aad9fc109a2cb
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-08 19:52:22 +0200

    Using useEffect with redux

--- 20-redux-side-effects-async/src/App.js
+++ 20-redux-side-effects-async/src/App.js
@@ -3,9 +3,19 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const { cartIsVisible } = useSelector((state) => state.ui);
+  const { cart } = useSelector((state) => state);
+
+  useEffect(() => {
+    fetch("https://arek-7fc06-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {cartIsVisible && <Cart />}

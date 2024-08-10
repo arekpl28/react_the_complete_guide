ae96dad435afde26388d8c0429a85b53baf10c43
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-10 19:01:58 +0200

    Navigating between pages with links

--- 21-SPA-react-router/src/pages/Home.js
+++ 21-SPA-react-router/src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>My home page</h1>;
+  return (
+    <>
+      <h1>My home page</h1>
+      <p>
+        <Link to="/products">Products</Link>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;

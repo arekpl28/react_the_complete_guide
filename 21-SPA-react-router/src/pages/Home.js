ad5a4aad89eb43dc96b8dbb8792fcc6e4405befd
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-10 20:17:27 +0200

    Navigating programmatically

--- 21-SPA-react-router/src/pages/Home.js
+++ 21-SPA-react-router/src/pages/Home.js
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandle() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h1>My home page</h1>
+      <button onClick={navigateHandle}>Navigate</button>
     </>
   );
 }

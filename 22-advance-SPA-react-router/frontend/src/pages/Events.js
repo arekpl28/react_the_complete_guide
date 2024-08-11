e160a131a061b998d7c4817de6c1dd35afe989c0
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-11 19:24:39 +0200

    Main finish starting project

--- 22-advance-SPA-react-router/frontend/src/pages/Events.js
+++ 22-advance-SPA-react-router/frontend/src/pages/Events.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  { id: "e1", title: "Some event" },
+  { id: "e2", title: "Another event" },
+];
+
 function EventsPage() {
-  return <h1>EventsPage</h1>;
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((item) => (
+          <li key={item.id}>
+            <Link to={item.id}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;

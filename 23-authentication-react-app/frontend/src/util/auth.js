86b603f721437536325244b0d5b17a5a1870d4d3
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-14 20:42:54 +0200

    Managing the token expiration

--- 23-authentication-react-app/frontend/src/util/auth.js
+++ 23-authentication-react-app/frontend/src/util/auth.js
@@ -1,7 +1,26 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const currentTime = new Date();
+  const duration = expirationDate.getTime() - currentTime.getTime();
+
+  return duration;
+}
 function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 }
 

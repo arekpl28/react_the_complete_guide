2258259f8cd9676eba1375f8e1109ba178542db4
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-14 20:16:11 +0200

    Adding route protection

--- 23-authentication-react-app/frontend/src/util/auth.js
+++ 23-authentication-react-app/frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 function getAuthToken() {
   const token = localStorage.getItem("token");
   return token;
@@ -7,4 +9,14 @@ export default getAuthToken;
 
 export function tokenLoader() {
   return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect("/auth");
+  }
+
+  return null;
 }

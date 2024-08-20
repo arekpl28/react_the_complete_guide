f1ad550e5086f328ed4d2e3aa2d7123db3ac7aec
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-20 20:14:29 +0200

    Relative & absolute paths

--- 28-SPA-react/src/pages/ProductDetail.js
+++ 28-SPA-react/src/pages/ProductDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function ProductDetailPage() {
   const params = useParams().productId;
@@ -7,6 +7,11 @@ function ProductDetailPage() {
     <>
       <h1>Product Detail</h1>
       <p>{params}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
     </>
   );
 }

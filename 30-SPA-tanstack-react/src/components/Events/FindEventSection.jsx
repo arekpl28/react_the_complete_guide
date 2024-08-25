367448f6ae61afc4187d6a35126eca5d3453004e
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-08-25 19:25:38 +0200

    Enabled & disabled queries

--- 30-SPA-tanstack-react/src/components/Events/FindEventSection.jsx
+++ 30-SPA-tanstack-react/src/components/Events/FindEventSection.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useRef, useState } from "react";
+
 import { fetchEvents } from "../../util/http";
 import LoadingIndicator from "../UI/LoadingIndicator";
 import ErrorBlock from "../UI/ErrorBlock";
@@ -7,11 +8,12 @@ import EventItem from "./EventItem";
 
 export default function FindEventSection() {
   const searchElement = useRef();
-  const { searchTerm, setSearchTerm } = useState("");
+  const [searchTerm, setSearchTerm] = useState();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["events", { search: searchTerm }],
     queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    enabled: searchTerm !== undefined,
   });
 
   function handleSubmit(event) {
@@ -21,7 +23,7 @@ export default function FindEventSection() {
 
   let content = <p>Please enter a search term and to find events.</p>;
 
-  if (isPending) {
+  if (isLoading) {
     content = <LoadingIndicator />;
   }
 
